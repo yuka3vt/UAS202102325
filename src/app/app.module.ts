6612f56c905428466c636f1fbd189e3551dba0e1
yuka3vt <yukawardana587@gmail.com>
2023-01-17 12:55:37 +0700

    UAS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
-import { NgModule, isDevMode } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MahasiswaComponent } from './mahasiswa/mahasiswa.component';
